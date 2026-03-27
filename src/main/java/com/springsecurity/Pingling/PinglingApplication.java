c95848cfb345f9a853645b5250d7e1f9c7315489
Rithikred <rithikreddy0210@email.com>
2026-03-26 23:45:58 -0400

    Initial Spring Boot security setup

---
+++ src/main/java/com/springsecurity/Pingling/PinglingApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurity.Pingling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinglingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinglingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springsecurity/Pingling/PinglingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurity.Pingling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PinglingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
