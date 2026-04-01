712dc7a14f297e597861ef5045b1028555881acd
Rithikred <rithikreddy0210@email.com>
2026-04-01 01:12:10 -0400

    Protect some endpoint with authentication and some was permitted

--- src/main/java/com/springsecurity/Pingling/config/ProjectSecurityConfig.java
+++ src/main/java/com/springsecurity/Pingling/config/ProjectSecurityConfig.java
@@ -1,8 +1,24 @@
 package com.springsecurity.Pingling.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectSecurityConfig {
 
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
+//        http.authorizeHttpRequests((requests) -> ((AuthorizeHttpRequestsConfigurer.AuthorizedUrl) requests.anyRequest()).authenticated());
+        http.authorizeHttpRequests((requests)->requests.requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated());
+        http.authorizeHttpRequests((requests)-> requests.requestMatchers("/notices","/contact","/error").permitAll());
+        http.formLogin(Customizer.withDefaults());
+        http.httpBasic(Customizer.withDefaults());
+        return (SecurityFilterChain) http.build();
+    }
 }
